f56a8e2911fc8d9678239b55ff57e5223fbcc5cb
HelloCostel <me@hellocostel.com>
2025-03-25 09:43:00 +0100

    Make characters animate one at time

--- script.js
+++ script.js
@@ -12,32 +12,48 @@ THEN WILL CHANGE ONE AT TIME IN THE PROPER LETTER.*/
 
 //define animation function
 const handleClick = () => {
-    const text = textInput.value;
-    const textLength = text.length;
-    //Generate symbols for any text-caracther
-    let intervalTimes = 6;
-    const animationInterval = setInterval(() => {
-        intervalTimes--;
-        console.log(intervalTimes);
+    //create an array of the string to iterate each letter
+    const inputString = textInput.value;
+    const lettersArray = inputString.split('');
 
-        if (intervalTimes === 0) {
-            console.log('end ' + text);
-            textGenerator.innerHTML = text;
-            clearInterval(animationInterval);
+    //array of symbols to display in DOM
+    const cryptedArray = [];
+    const setCryptedArray = () => {
+        for (let i = 0; i < lettersArray.length; i++) {
+            cryptedArray.push(symbols[Math.floor(Math.random() * symbolsLength)]);
         }
+        console.log('first array: ' + cryptedArray);
+        textGenerator.innerHTML = cryptedArray.join('');
+    }
+    setCryptedArray();
 
-        if (intervalTimes !== 0) {
-            let cryptoText = [];
-        for (let i = 0; i < textLength; i++) {
-            //Take a random symbol from symbols and push it to cryptoText
-            const index = Math.floor(Math.random() * symbolsLength);
-            cryptoText.push(symbols[index]);
+    //animate the symbols string
+    const delay = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    async function processArray(array) {
+        for (const letter of lettersArray) {
+            //index of current letter
+            const index = lettersArray.indexOf(letter);
+            //add random symbol at proper index instead of current letter
+            const symbolIndex = Math.floor(Math.random() * symbolsLength);
+            const cryptedLetter = symbols[symbolIndex];
+            cryptedArray[index] = cryptedLetter;
+            console.log('new array: ' + cryptedArray);
+            //display the new string
+            textGenerator.innerHTML = cryptedArray.join('');
+            //await N ms to iterate
+            await delay(1000); 
         }
-        //Display the string into DOM
-        const string = cryptoText.join('');
-        textGenerator.innerHTML = string;
-        }
-    }, 500)
+    }
+
+    processArray(lettersArray);
+
+
+    const stopInterval = () => {
+        clearInterval(animationInterval);
+    }
+    
 };
 
 button.addEventListener('click', handleClick);
