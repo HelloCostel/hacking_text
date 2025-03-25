cdc3865f20981dd169c3a9af54b5f390cdeba6fb
HelloCostel <me@hellocostel.com>
2025-03-25 08:20:59 +0100

    Repeat the animation a specified value of times

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const button = document.querySelector('.button');
 //define animation settings
 const symbols = ['!', '"', '£', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
 const symbolsLength = symbols.length;
-const time = 1500;
 
 /*UNTIL THE END OF TIME, CARACTHERS WILL CHANGE ONE AT TIME IN A SYMBOL.
 THEN WILL CHANGE ONE AT TIME IN THE PROPER LETTER.*/
@@ -16,18 +15,29 @@ const handleClick = () => {
     const text = textInput.value;
     const textLength = text.length;
     //Generate symbols for any text-caracther
-    let cryptoText = [];
-    for (let i = 0; i < textLength; i++) {
-        //Take a random symbol from symbols and push it to cryptoText
-        const index = Math.floor(Math.random() * symbolsLength);
-        cryptoText.push(symbols[index]);
-        console.log(cryptoText.join(''));
-    }
-    //Display the string into DOM
-    let string = cryptoText.join('');
-    textGenerator.innerHTML = string;
+    let intervalTimes = 6;
+    const animationInterval = setInterval(() => {
+        intervalTimes--;
+        console.log(intervalTimes);
 
-    // textGenerator.innerHTML = text;
+        if (intervalTimes === 0) {
+            console.log('end ' + text);
+            textGenerator.innerHTML = text;
+            clearInterval(animationInterval);
+        }
+
+        if (intervalTimes !== 0) {
+            let cryptoText = [];
+        for (let i = 0; i < textLength; i++) {
+            //Take a random symbol from symbols and push it to cryptoText
+            const index = Math.floor(Math.random() * symbolsLength);
+            cryptoText.push(symbols[index]);
+        }
+        //Display the string into DOM
+        const string = cryptoText.join('');
+        textGenerator.innerHTML = string;
+        }
+    }, 500)
 };
 
 button.addEventListener('click', handleClick);
